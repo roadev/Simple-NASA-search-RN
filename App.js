ba49876c55ea6db29b9a82672b69a426d95fba38
Juan Roa <jdroa92@gmail.com>
2018-06-30 09:34:44 -0500

    creating counter component

--- App.js
+++ App.js
@@ -1,9 +1,45 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, Button } from 'react-native';
+
+class Counter extends Component {
+  state = {
+    count: this.props.count
+  };
+
+  handleOnClick = () =>
+    this.setState({ count: this.state.count + 1 });
+
+  render() {
+    return (
+      <View>
+        <Text>The count is: {this.state.count}</Text>
+        <Button onPress={this.handleOnClick}
+          title="Click me"
+        />
+      </View>
+    );
+  }
+}
+
+const SuperCount = () => (
+  <View>
+    <Counter count={1} />
+  </View>
+);
 
 const App = () => (
-  <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+  <View
+    style={
+      {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }
+    }
+  >
     <Text>Hello RN!!!!!</Text>
+    <SuperCount />
   </View>
 )
 
