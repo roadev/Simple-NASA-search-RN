146fbe3f5e6ea1aae3d77c3c150be1cb81c0a1df
Juan Roa <jdroa92@gmail.com>
2018-06-30 08:32:45 -0500

    initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => (
+  <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Hello RN!!!!!</Text>
+  </View>
+)
+
+
+export default App;
